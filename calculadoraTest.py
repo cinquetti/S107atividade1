9129ed5ee8025a8da9d1c6c660576f7931c3d2e5
cinquetti <lucas.cinquetti@ges.inatel.br>
2024-03-21 16:43:35 -0300

    Testes modificados

--- calculadoraTest.py
+++ calculadoraTest.py
@@ -1,8 +1,30 @@
 import unittest
+from calculadora import soma, subtracao, multiplicacao, divisao
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCalculadora(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(2, 3), 5)
+        self.assertEqual(soma(-1, 1), 0)
+        self.assertEqual(soma(0, 0), 0)
+
+    def test_subtracao(self):
+        self.assertEqual(subtracao(5, 2), 3)
+        self.assertEqual(subtracao(-1, -1), 0)
+        self.assertEqual(subtracao(0, 0), 0)
+
+    def test_multiplicacao(self):
+        self.assertEqual(multiplicacao(2, 3), 6)
+        self.assertEqual(multiplicacao(-1, 1), -1)
+        self.assertEqual(multiplicacao(0, 5), 0)
+
+    def test_divisao(self):
+        self.assertEqual(divisao(6, 3), 2)
+        self.assertEqual(divisao(10, 2), 5)
+        self.assertEqual(divisao(0, 5), 0)
+
+    def test_divisao_por_zero(self):
+        self.assertEqual(divisao(6, 0), "Não é possivel dividir por zero")
+        self.assertEqual(divisao(10, 0), "Não é possivel dividir por zero")
 
 if __name__ == '__main__':
     unittest.main()
