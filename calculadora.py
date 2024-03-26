9f6474d7d1f8a89bd5d5cde9fca0bdaf0939b298
CarlBetsa <carl.betsa@ges.inatel.br>
2024-03-26 14:46:46 -0300

    Att calculadora

--- calculadora.py
+++ calculadora.py
@@ -13,7 +13,28 @@ def divisao(a, b):
     else:
         return "Não é possivel dividir por zero"
 
-def calculadora():
+def calculadora(escolha):
+    
+    if escolha not in ['1', '2', '3', '4']:
+        return ("Escolha inválida!")
+    num1 = input("Digite o primeiro número: ")
+    num2 = input("Digite o segundo número: ")
+
+    if num1.isnumeric() and num2.isnumeric():
+        num1 = float(num1)
+        num2 = float(num2)
+    else: return "Entrada inválida!"
+
+    if escolha == '1':
+        return soma(num1, num2)
+    elif escolha == '2':
+        return subtracao(num1, num2)
+    elif escolha == '3':
+        return multiplicacao(num1, num2)
+    elif escolha == '4':
+        return divisao(num1, num2)
+    
+def run_calculadora():
     print("Selecione o número da operação desejada:")
     print("1- Soma")
     print("2- Subtração")
@@ -21,19 +42,6 @@ def calculadora():
     print("4- Divisão")
 
     escolha = input("Digite sua escolha (1/2/3/4): ")
+    print(calculadora(escolha))
 
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
-
-    if escolha == '1':
-        print("Resultado:", soma(num1, num2))
-    elif escolha == '2':
-        print("Resultado:", subtracao(num1, num2))
-    elif escolha == '3':
-        print("Resultado:", multiplicacao(num1, num2))
-    elif escolha == '4':
-        print("Resultado:", divisao(num1, num2))
-    else:
-        print("Escolha inválida!")
-
-calculadora()
+run_calculadora()
